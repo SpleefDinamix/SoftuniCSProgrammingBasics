67f48da6d69c215d98d765a0748a331d2c2d55be
Spleef Dinamix <pepi15032000@gmail.com>
2018-05-29 15:53:42 +0300

    Failsave

---
+++ DrawingFiguresWithLoops/08.Sunglasses/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _08.Sunglasses
+{
+    class Program
+    {
+        static void Main()
+        {
+            int frameHeight = int.Parse(Console.ReadLine());
+            int frameWidth = frameHeight * 2;
+
+            CreateTopBottomFrame(frameWidth, frameHeight);
+            for (int i = 0; i < frameHeight-2; i++)
+            {
+                DrawACenterPart(frameWidth, frameHeight, i);
+            }
+            CreateTopBottomFrame(frameWidth, frameHeight);
+        }
+
+        public static void DrawACenterPart(int width, int spacing, int index)
+        {
+            int bridgeIndex = (int)((spacing - 2) / 2d);
+            if ((spacing - 2) % 2 == 0)
+            {
+                bridgeIndex--;
+            }
+
+            var fillPart = new String('/', width - 2);
+            var spacingPart = new String(' ', spacing);
+
+            if (index == bridgeIndex)
+            {
+                spacingPart = new String('|', spacing);
+            }
+            Console.WriteLine('*' + fillPart + '*' + spacingPart + '*' + fillPart + '*');
+        }
+
+        public static void CreateTopBottomFrame(int width, int spacing)
+        {
+            var sidePart = new String('*', width);
+            var spacingPart = new String(' ', spacing); 
+            Console.WriteLine(sidePart + spacingPart + sidePart);
+        }
+    }
+}
---
+++ DrawingFiguresWithLoops/09.House/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _09.House
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int roofSize = (n + 1) / 2;
+            int wallSize = n / 2 - 1;
+
+            int stars = n % 2 == 0 ? 2 : 1 ;
+
+            for (int i = 0; i < roofSize; i++)
+            {
+                var dashes = new String('-', (n - stars)/2);
+                Console.WriteLine(dashes + new String('*',stars) + dashes);
+                stars += 2;
+            }
+
+        }
+    }
+}
